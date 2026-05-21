9038c23a92643b76f6a390d1aa243adb4120dea8
sutharpankaj1 <sutharpan6004@gmail.com>
2026-05-21 12:24:30 +0530

    Task1 Bubble sort Descending order using c#

--- Sorting/BubbleSort.cs
+++ Sorting/BubbleSort.cs
@@ -2,6 +2,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ascending order
         int[] arr=[64,34,25,12,22,11,90,5];
         for(int i=0;i<arr.Length-1; i++)
         {
@@ -18,7 +19,28 @@ class Program
 
         foreach(int i in arr)
         {
-            Console.WriteLine(i);
+            Console.Write(i+" ");
+        }
+        Console.WriteLine();
+
+        //Descending order
+        int[] arr1=[64,34,25,12,22,11,90,5];
+        for(int i =arr1.Length-1; i>0;i--)
+        {
+            for(int j =arr1.Length-1; j>0;j--)
+            {
+                if(arr1[j]>arr1[j-1])
+                {
+                    int temp=arr1[j];
+                    arr1[j]=arr1[j-1];
+                    arr1[j-1]=temp;
+                }
+            }
+        }
+
+        foreach(int i in arr1)
+        {
+            Console.Write(i+" ");
         }
     }
 }
