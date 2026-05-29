9e9d9869086889bec60c022a514162b1000296f1
sutharpankaj1 <sutharpan6004@gmail.com>
2026-05-29 17:09:42 +0530

    Task Add TreeNode class with node counting and summation functionality

---
+++ tree/SumNode.cs
@@ -0,0 +1,43 @@
+class TreeNode
+{
+    public int Value;
+
+    public TreeNode Left;
+    public TreeNode Right;
+    public TreeNode(int value)
+    {
+        Value = value;
+    }
+
+    public int CountNodes(TreeNode node)
+    {
+        if (node == null)
+            return 0;
+
+        return 1 + CountNodes(node.Left) + CountNodes(node.Right);
+    }
+
+    public int Sum(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        return root.Value + Sum(root.Left) + Sum(root.Right);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TreeNode root = new TreeNode(10);
+        root.Left = new TreeNode(20);
+        root.Right = new TreeNode(30);
+        Console.WriteLine(root.Value);
+        Console.WriteLine(root.Left.Value);
+        Console.WriteLine(root.Right.Value);
+
+        Console.WriteLine("Total number of nodes in the tree: " + root.CountNodes(root));
+        Console.WriteLine("Sum of all nodes in the tree: " + root.Sum(root));
+    }
+}
